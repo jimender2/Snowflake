fb5505c1eed62ce98e90833a3fe40410a75a9385
BuildTools <unconfigured@null.spigotmc.org>
2018-01-24 08:42:21 -0500

    Snowflake
    Snowflake

---
+++ src/Snowflake.java
@@ -0,0 +1,18 @@
+//Jonathan Meredith
+public class Snowflake 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("\t    -\\\\/      \\//-");
+		System.out.println("\t     -\\\\/    \\//-");
+		System.out.println("\t      -\\\\/  \\//-");
+		System.out.println("\t       -\\\\/\\//-");
+		System.out.print("\t-\\-\\-\\-\\-//\\\\-/-/-/-/-\n"
+				+ "\t-/-/-/-/-\\\\//-\\-\\-\\-\\-\n");
+		System.out.println("\t       -//\\/\\\\-");
+		System.out.println("\t      -//\\  /\\\\-");
+		System.out.println("\t     -//\\    /\\\\-");
+		System.out.println("\t    -//\\      /\\\\-");
+	}
+}
